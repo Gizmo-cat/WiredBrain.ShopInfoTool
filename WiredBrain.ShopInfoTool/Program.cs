317cde04086950d8c8ba63b643a3b20b9cde20ef
Gizmo-cat <nicholasscott@outlook.com>
2021-06-19 12:33:07 +0100

    Add project files.

---
+++ WiredBrain.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrain.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the WiredBrain ShopInfoTool");
+        }
+    }
+}
