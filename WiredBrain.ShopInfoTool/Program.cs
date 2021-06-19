c2ebb172604d44b92f5dc7a22c823f17523c635a
Gizmo-cat <nicholasscott@outlook.com>
2021-06-19 14:18:09 +0100

    Implement help command

--- WiredBrain.ShopInfoTool/Program.cs
+++ WiredBrain.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrain.ShopInfoTool
 {
@@ -7,6 +8,21 @@ namespace WiredBrain.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the WiredBrain ShopInfoTool 2021 June");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
