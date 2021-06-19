b160aca7ce6943c9355ba228ac0e17a70ad446b0
Gizmo-cat <nicholasscott@outlook.com>
2021-06-19 15:03:31 +0100

    NS Changed the quite message anytime

--- WiredBrain.ShopInfoTool/Program.cs
+++ WiredBrain.ShopInfoTool/Program.cs
@@ -9,7 +9,7 @@ namespace WiredBrain.ShopInfoTool
         {
             Console.WriteLine("Welcome to the WiredBrain ShopInfoTool 2021 June");
 
-            Console.WriteLine("Write 'help' to list available commands, write 'quit' to exit application");
+            Console.WriteLine("Write 'help' to list available commands, write 'quit' anytime to exit application");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
             while (true)
