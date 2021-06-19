e8de7810a8d5d79e83ec1477e29e947ee3fbb5a5
Nicholas Scott <nicholasscott@outlook.com>
2021-06-19 16:40:29 +0100

    fixed added Id

--- WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -4,6 +4,7 @@ namespace WiredBrainCoffee.DataAccess.Model
 {
     public class CoffeeShop
     {
+        public int Id { get; set; }
         public string Location { get; set; }
         public int BeansInStockInKg { get; set; }
     }
